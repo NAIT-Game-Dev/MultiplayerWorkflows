076505547b119afc2a738e6bf33b3807d0eb5008
John Winski <JWINSKI@nait.ca>
2025-03-31 09:53:52 -0600

    Add project files.

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject spawnPoint;
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] List<GameObject> players;
+    [SerializeField] List<int> gamepadID;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If a new gamepad gets plugged in.
+        if (Gamepad.all.Count > gamepadID.Count)
+        {
+            // Spawn a player object for the new gamepad
+            GameObject newPlayer = Instantiate(playerPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+            // Add the new player object to a list
+            players.Add(newPlayer);
+
+            // Set the gamepadID for the player to be the one that just got plugged in
+            newPlayer.GetComponent<W1_PlayerMovement>().SetGamepadID(Gamepad.all[players.Count - 1].deviceId);
+
+            // Add the gamepadID to a list
+            gamepadID.Add(Gamepad.all[players.Count - 1].deviceId);
+            UpdatePlayerID();
+
+        }
+
+        // If all gamepads are disconnected then clear the lists and destroy the last player object
+        if (Gamepad.all.Count == 0 && gamepadID.Count > 0)
+        {
+            gamepadID.Clear();
+            Destroy(players[0]);
+            players.Clear();
+        }
+        
+        // If a gamepad got disconnected then find the player object that was connected to it
+        if (Gamepad.all.Count < gamepadID.Count)
+        {
+            // Go through the list of gamepadIDs
+            for (int i = 0; i < gamepadID.Count; i++)
+            {
+                bool found = false;
+                // Search throught the current gamepads that are connected
+                for (int j = 0; j < Gamepad.all.Count; j++)
+                {
+                    // If the gamepadID is in the list of gamepads connected then you found it and do nothing
+                    if (gamepadID[i] == Gamepad.all[j].deviceId)
+                    {
+                        found = true; 
+                    }
+                }
+                // If the gamepadID was not found then remove at that index from both lists and destroy the player object
+                if (!found)
+                {
+                    gamepadID.RemoveAt(i);                    
+                    players.RemoveAt(i);
+                    Destroy(players[i]);
+                }
+            }
+            // Update the playerIDs so that all gamepads will control the player objects that they did before
+            UpdatePlayerID();
+        }
+    }
+
+    void UpdatePlayerID()
+    {
+        for (int i = 0; i < gamepadID.Count; i++)
+        {
+            for (int j = 0; j < players.Count; j++)
+            {
+                if (players[j].GetComponent<W1_PlayerMovement>().GetGamepadID() == gamepadID[i])
+                {
+                    players[j].GetComponent<W1_PlayerMovement>().SetPlayerID(i);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/W1_PlayerMovement.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class W1_PlayerMovement : MonoBehaviour
+{
+    float movementSpeed = 3.0f;
+
+    [SerializeField] int playerID, gamepadID;
+
+    float forward, backward, right, left, xValue, zValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if you gamepads are connected then any player objects will be controlled by the keyboard. This is mostly for testing purposes as you will need to put a prefab in the scene.
+        #region Keyboard Input
+        //if (Gamepad.all.Count == 0)
+        //{
+        //    if (Keyboard.current.wKey.isPressed)
+        //    {
+        //        forward = 1;
+        //    }
+        //    else
+        //    {
+        //        forward = 0;
+        //    }
+
+        //    if (Keyboard.current.sKey.isPressed)
+        //    {
+        //        backward = 1;
+        //    }
+        //    else
+        //    {
+        //        backward = 0;
+        //    }
+
+        //    if (Keyboard.current.aKey.isPressed)
+        //    {
+        //        left = 1;
+        //    }
+        //    else
+        //    {
+        //        left = 0;
+        //    }
+
+        //    if (Keyboard.current.dKey.isPressed)
+        //    {
+        //        right = 1;
+        //    }
+        //    else
+        //    {
+        //        right = 0;
+        //    }
+        //    xValue = right - left;
+        //    zValue = forward - backward;
+        //}
+        #endregion
+
+        #region Controller Input for Gamepad
+        // playerID is the index of the gamepad in the Gamepad list. Make sure the playerID does not exceed the number of gamepads plugged in.
+        if (Gamepad.all.Count > playerID)
+        {
+            #region IsPressed
+            // use the controls of the gamepad in the list based on the playerID index. In this example we are setting values based on whether the control is pressed.
+            //if (Gamepad.all[playerID].leftStick.up.isPressed)
+            //{
+            //    forward = 1;
+            //}
+            //else
+            //{
+            //    forward = 0;
+            //}
+
+            //if (Gamepad.all[playerID].leftStick.down.isPressed)
+            //{
+            //    backward = 1;
+            //}
+            //else
+            //{
+            //    backward = 0;
+            //}
+
+            //if (Gamepad.all[playerID].leftStick.right.isPressed)
+            //{
+            //    right = 1;
+            //}
+            //else
+            //{
+            //    right = 0;
+            //}
+
+            //if (Gamepad.all[playerID].leftStick.left.isPressed)
+            //{
+            //    left = 1;
+            //}
+            //else
+            //{
+            //    left = 0;
+            //}
+
+            //xValue = right - left;
+            //zValue = forward - backward;
+            #endregion
+
+            #region Value
+            // use the controls of the gamepad in the list based on the playerID index. In this example we are getting the float values of the left stick controls.
+            forward = Gamepad.all[playerID].leftStick.up.value;
+            backward = Gamepad.all[playerID].leftStick.down.value;
+            right = Gamepad.all[playerID].leftStick.right.value;
+            left = Gamepad.all[playerID].leftStick.left.value;
+
+            xValue = right - left;
+            zValue = forward - backward;
+            #endregion
+        }
+        #endregion        
+    }
+
+    private void FixedUpdate()
+    {
+        // move the object based on the values of the gamepad
+        transform.Translate(new Vector3(xValue * movementSpeed * Time.fixedDeltaTime, 0, zValue * movementSpeed * Time.fixedDeltaTime));
+    }
+
+    public void SetPlayerID(int id)
+    {
+        // set the player id to the index of the gamepad in use.
+        playerID = id;
+    }
+
+    public int GetGamepadID()
+    {
+        // return the gamepad ID that the object is currently linked to.
+        return gamepadID;
+    }
+
+    public void SetGamepadID(int id)
+    {
+        // set the gamepad ID that the object should be linked to.
+        gamepadID = id;
+    }
+}
