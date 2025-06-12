609ed53d45e084cb026ca43eaf5f40de5f95ccac
John Winski <JWINSKI@nait.ca>
2025-06-12 08:56:56 -0600

    Tried to use embedded actions with multiple controllers. Kinda working.

--- Assets/W2_PlayerMovement.cs
+++ Assets/W2_PlayerMovement.cs
@@ -7,7 +7,11 @@ public class W2_PlayerMovement : MonoBehaviour
 
     [SerializeField] int gamepadID, playerIndex;
 
-    float forward, backward, right, left, xValue, zValue;
+    [SerializeField] InputAction moveAction;
+
+    [SerializeField] Vector2 moveValue;
+
+    float xValue, zValue;
 
     // Update is called once per frame
     void Update()
@@ -15,16 +19,15 @@ public class W2_PlayerMovement : MonoBehaviour
         if (gamepadID != -1)
         {
             #region Controller Input for Gamepad
-            // playerID is the index of the gamepad in the Gamepad list. Make sure the playerID does not exceed the number of gamepads plugged in.
-
-            // use the controls of the gamepad in the list based on the playerID index. In this example we are getting the float values of the left stick controls.
-            forward = Gamepad.all[playerIndex].leftStick.up.value;
-            backward = Gamepad.all[playerIndex].leftStick.down.value;
-            right = Gamepad.all[playerIndex].leftStick.right.value;
-            left = Gamepad.all[playerIndex].leftStick.left.value;
-
-            xValue = right - left;
-            zValue = forward - backward;
+            if (Gamepad.current.deviceId == gamepadID)
+            {
+                Debug.Log(Gamepad.current.deviceId);
+                moveValue = moveAction.ReadValue<Vector2>();
+            }
+            else
+            {
+                moveValue = Vector2.zero;
+            }
             #endregion
         }
     }
@@ -32,7 +35,7 @@ public class W2_PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // move the object based on the values of the gamepad
-        transform.Translate(new Vector3(xValue * movementSpeed * Time.fixedDeltaTime, 0, zValue * movementSpeed * Time.fixedDeltaTime));
+        transform.Translate(new Vector3(moveValue.x * movementSpeed * Time.fixedDeltaTime, 0, moveValue.y * movementSpeed * Time.fixedDeltaTime));
     }
 
     public int GetGamepadID()
@@ -45,5 +48,15 @@ public class W2_PlayerMovement : MonoBehaviour
     {
         // set the gamepad ID that the object should be linked to.
         gamepadID = id;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
     }
 }
